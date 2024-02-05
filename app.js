3ca292c6d756c1dfd71a196e11c607fce4ec5acc
rakibshakib <rakibshakib74@gmail.com>
2024-02-05 23:10:42 +0600

    animation added

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     var audio = document.getElementById('backgroundAudio');
+const noButton = document.getElementById("no-btn");
+const yesButton = document.getElementById("yes-btn");
+const imgTag = document.getElementById("imgTag");
+const imageContainer = document.getElementsByClassName("image-section");
+const confusedImage = "./confued.gif";
+const joyImages = "./joy.gif";
+const sadImages = "./sad.gif";
+const requestingImag = "./requesting.gif";
+const yes = "./yes.gif";
 
-//     // Hide audio controls
-//     audio.controls = false;
-
-//     // Set visibility to none
-//     // audio.style.visibility = 'hidden';
-//   });
-
-const button = document.getElementById("click-btn");
-console.log("button: ", button);
+console.log("button: ", noButton);
 let timeoutId;
 function mouseTranformation(event) {
   console.log({ event });
+  imgTag.src = confusedImage;
   var randomX = Math.floor(Math.random() * 501) - 250; // Adjust the range based on desired movement
   var randomY = Math.floor(Math.random() * 401) - 200;
-  button.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
+  noButton.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
   clearStyle();
 }
 
 const clearStyle = () => {
   clearTimeout(timeoutId);
   timeoutId = setTimeout(() => {
-    button.style.transform = "translate(" + 0 + "px, " + 0 + "px)";
+    imgTag.src = requestingImag;
+    // noButton.style.transform = "translate(" + 0 + "px, " + 0 + "px)";
   }, 1000);
 };
 
-button.addEventListener("mouseenter", mouseTranformation);
-button.addEventListener("click", mouseTranformation);
-button.addEventListener("mouseover", mouseTranformation);
-button.addEventListener("mouseout", mouseTranformation);
+noButton.addEventListener("mouseenter", mouseTranformation);
+noButton.addEventListener("click", mouseTranformation);
+noButton.addEventListener("mouseover", mouseTranformation);
+noButton.addEventListener("mouseout", mouseTranformation);
+
+yesButton.addEventListener("click", () => {
+  console.log("yes button click porche");
+  imgTag.src = yes;
+});
+
+
+yesButton.addEventListener("mouseenter", () => {
+  imgTag.src = joyImages;
+  // clearStyle();
+});
+yesButton.addEventListener("mouseover", () => {
+  imgTag.src = joyImages;
+});
+yesButton.addEventListener("mouseout", () => {
+  clearStyle();
+});
