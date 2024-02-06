e02006c00e7e215a2cdaed7f6be2e34c37d9bd76
iboscopilot <eaccount@ibos.io>
2024-02-06 11:43:01 +0600

    update

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
-// window.addEventListener('DOMContentLoaded', (event) => {
-//     var audio = document.getElementById('backgroundAudio');
-
-//     // Hide audio controls
-//     audio.controls = false;
-
-//     // Set visibility to none
-//     // audio.style.visibility = 'hidden';
-//   });
-
 const button = document.getElementById("click-btn");
-console.log("button: ", button);
+const inputBox = document.getElementById("inputBox");
+
 let timeoutId;
 function mouseTranformation(event) {
-  console.log({ event });
   var randomX = Math.floor(Math.random() * 501) - 250; // Adjust the range based on desired movement
   var randomY = Math.floor(Math.random() * 401) - 200;
   button.style.transform = "translate(" + randomX + "px, " + randomY + "px)";
@@ -30,3 +20,13 @@ button.addEventListener("mouseenter", mouseTranformation);
 button.addEventListener("click", mouseTranformation);
 button.addEventListener("mouseover", mouseTranformation);
 button.addEventListener("mouseout", mouseTranformation);
+
+
+inputBox.addEventListener("change", (event)=> {
+  console.log("changing event ", event);
+  console.log(inputBox.value)
+});
+
+function onchangeInputHandler(event){
+  console.log("event", event)
+}
